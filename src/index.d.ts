f9fed591c6129b2e53845d3d112c7252dca2e917
Bee Clark <mrhumanman2@gmail.com>
2021-12-31 17:01:34 -0500

    Fill in KickTable, Offset, and KickData

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,31 +1,46 @@
 export class Engine {
-  private constructor(
-    width: number,
-    height: number,
-    pieces: Array<PieceData>,
-    numBoards?: number
-  );
+	private constructor(
+		width: number,
+		height: number,
+		pieces: Array<PieceData>,
+		numBoards?: number
+	);
 }
 
 export class Board {
-  private constructor(width: number, height: number);
+	private constructor(width: number, height: number);
 }
 
 export class Piece {
-  private constructor(data: PieceData, meta: Metadata);
+	private constructor(data: PieceData, meta: Metadata);
 }
 
 export class Square {
 	private constructor(position: Position, meta: Metadata);
 }
 
-export interface KickTable {}
+export interface KickTable {
+	"0": KickData;
+	"1": KickData;
+	"2": KickData;
+	"3": KickData;
+}
 
 export interface Position {
-  x: number;
-  y: number;
+	x: number;
+	y: number;
+}
+
+export interface Offset {
+	x: number;
+	y: number;
 }
 
 export interface PieceData {}
 
 export interface Metadata {}
+
+export interface KickData {
+	"+": Array<Offset>;
+	"-": Array<Offset>;
+}
